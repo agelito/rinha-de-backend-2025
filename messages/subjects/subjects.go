39b861c6c35d7f09fcced77c77dbaccff79629b8
Axel Wettervik <axel.wettervik@gmail.com>
2025-07-13 18:57:49 +0900

    feat: wait for payment processing confirmation

--- messages/subjects/subjects.go
+++ messages/subjects/subjects.go
@@ -1,5 +1,12 @@
 package subjects
 
+import "fmt"
+
 const (
-	SubjectPaymentsProcess = "payments.process"
+	SubjectPaymentsProcess    = "payments.process"
+	subjectPaymentsConfirmFmt = "payments.confirm.%s"
 )
+
+func NewPaymentsConfirmChannel(correlationId string) string {
+	return fmt.Sprintf(subjectPaymentsConfirmFmt, correlationId)
+}
