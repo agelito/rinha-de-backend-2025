14b2cc6a1271816edd59e6de5aa8d49d7870ef29
Axel Wettervik <axel.wettervik@gmail.com>
2025-07-14 21:30:15 +0900

    feat: containerize api and payment-worker services
    add the services to docker-compose file using nginx as a load balancer

--- api/main.go
+++ api/main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
-	"log"
+	"os"
 
 	"github.com/agelito/rinha-de-backend-2025/api/pkg/handler"
 	"github.com/agelito/rinha-de-backend-2025/api/pkg/service"
+	"github.com/charmbracelet/log"
 	"github.com/nats-io/nats.go"
 )
 
 func main() {
-	nc, err := nats.Connect(nats.DefaultURL)
+	natsUrl := nats.DefaultURL
+
+	if envUrl := os.Getenv("NATS_URL"); envUrl != "" {
+		natsUrl = envUrl
+	}
+
+	log.Info("connecting to nats", "url", natsUrl)
+	nc, err := nats.Connect(natsUrl)
 
 	if err != nil {
 		log.Fatal(err)
